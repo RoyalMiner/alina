23ae3f5984eb3aef197918d620131d8af8c8a48c
royal_miner <1899pablo@gmail.com>
2022-04-19 17:10:39 +0200

    alhamdudile

---
+++ mysql.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$name = "s3425_wiedelustigleckerbist";
+$user = "s3425_wiedelustigbist";
+$passwort = "Ya6s60?b";
+try{
+    $mysql = new PDO("mysql:host=$host;dbname=$name", $user, $passwort);
+} catch (PDOException $e){
+    echo "SQL Error: ".$e->getMessage();
+}
+ ?>
